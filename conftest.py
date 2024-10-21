21b281f80d0222f28cab2d54863a4db7a687d058
AlexanderRedekop <redekopaleksandr7@gmail.com>
2024-10-21 01:00:01 +0000

    Add tests

--- conftest.py
+++ conftest.py
@@ -5,9 +5,7 @@ from selenium.webdriver.chrome.options import Options
 # Создание предусловия. (Открытие браузера и сайта)
 @pytest.fixture()
 def driver():
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
     driver.maximize_window()    # Полный размер экрана у браузера.
     driver.implicitly_wait(10)  # Неявное ожидание
     driver.get('https://candymapper.com/')  # Открытие сайта
